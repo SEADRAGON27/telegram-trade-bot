00412b984af63fe3946b232eb2720ddd97ddda30
SEADRAGON27 <121949031+SEADRAGON27@users.noreply.github.com>
2023-09-19 21:18:07 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,42 +1,28 @@
 import "dotenv/config";
 import { MongoClient } from "mongodb";
 
-export const extractFromDB = async (collectionName, data) => {
+export const DB = async (method, data, collection, filter) => {
   const client = new MongoClient(process.env.CONECTION_DB);
   await client.connect();
   const db = client.db("TelegramBot");
-  const collection = db.collection(collectionName);
-  const result = await collection.find(data).toArray();
-  client.close();
-  return result;
-};
-
-export const recordToDB = async (data, collectionName) => {
-  const client = new MongoClient(process.env.CONECTION_DB);
-  await client.connect();
-  const db = client.db("TelegramBot");
-  const collection = db.collection(collectionName);
-  await collection.insertOne(data);
-  client.close();
-};
-
-export const updateDataDB = async (filter, data, collectionName) => {
-  const client = new MongoClient(process.env.CONECTION_DB);
-  await client.connect();
-  const db = client.db("TelegramBot");
-  const collection = db.collection(collectionName);
-  await collection.updateOne(filter, data);
+  const collection = db.collection(collection);
+  switch (method) {
+    case "getData":
+      const result = await collection.find(data).toArray();
+      return result;
+    case "recordData":
+      await collection.insertOne(data);
+      break;
+    case "updateData":
+      await collection.updateOne(filter, data);
+      break;
+    case "deleteData":
+      await collection.deleteOne(data);
+      break;
+  }
   client.close();
 };
 
-export const deleteFromDB = async (data, collectionName) => {
-  const client = new MongoClient(process.env.CONECTION_DB);
-  await client.connect();
-  const db = client.db("TelegramBot");
-  const collection = db.collection(collectionName);
-  await collection.deleteOne(data);
-  client.close();
-};
 
 export const findUserInfo = (userId) => {
   return {
