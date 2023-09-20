205cc99d24eeb39fece6c96c732791baa588a163
SEADRAGON27 <121949031+SEADRAGON27@users.noreply.github.com>
2023-09-20 20:36:21 +0300

    Update priceCurrancy.js

--- src/tradingScenes/priceCurrancy.js
+++ src/tradingScenes/priceCurrancy.js
@@ -2,18 +2,18 @@ import { Scenes } from "telegraf";
 import { pricesCryptoCurrancy } from "../../api.js";
 export const wizardScenePrice = new Scenes.WizardScene(
   "getPricesCrypto",
-  (ctx) => {
-    ctx.reply("✍ Write cryptocurrencies name(bitcoin,monero,ripple)");
+  async (ctx) => {
+    await ctx.reply("✍ Write cryptocurrencies name(bitcoin,monero,ripple)");
     ctx.wizard.next();
   },
   async (ctx) => {
     const userPrompt = ctx.message.text;
     const res = await pricesCryptoCurrancy(userPrompt);
     if (+res) {
-      ctx.reply(`Price:${res}`);
+      await ctx.reply(`Price:${res}`);
       ctx.scene.leave();
     } else {
-      ctx.reply("⛔Cryptocurrency not found, please re-enter the text");
+     await ctx.reply("⛔Cryptocurrency not found, please re-enter the text");
     }
   }
 );
