cf97fa52f0e13a2e3b38c2dbd6fa82b2ab703ec0
SEADRAGON27 <121949031+SEADRAGON27@users.noreply.github.com>
2023-09-20 20:48:20 +0300

    Update cancelOrders.js

--- src/tradingScenes/cancelOrders.js
+++ src/tradingScenes/cancelOrders.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { Markup, Scenes } from "telegraf";
 import { listAllOrders, cancelOrder } from "../../api.js";
-import { extractFromDB, findUserInfo } from "../db.js";
+import { DB, findUserInfo } from "../db.js";
 import { getDate } from "../date.js";
 export const cancelOrdersScene = new Scenes.WizardScene(
   "cancelOrders",
