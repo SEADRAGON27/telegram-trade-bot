c6a499340323d3cc92d6d8e1324405f710207a22
SEADRAGON27 <121949031+SEADRAGON27@users.noreply.github.com>
2023-09-20 20:38:36 +0300

    Update withdraw.js

--- src/tradingScenes/withdraw.js
+++ src/tradingScenes/withdraw.js
@@ -4,28 +4,28 @@ import { withdraw } from "../../api.js";
 import { extractFromDB, findUserInfo } from "../db.js";
 export const withdrawScene = new Scenes.WizardScene(
   "withdraw",
-  (ctx) => {
-    ctx.reply("✍Write cryptocurrency which do you want withdraw:");
+  async (ctx) => {
+    await ctx.reply("✍Write cryptocurrency which do you want withdraw:");
     ctx.wizard.next();
   },
-  (ctx) => {
+  async (ctx) => {
     const currancy = ctx.message.text;
     const symbolRegex = /^[A-Z]{3,}$/;
     if (symbolRegex(currancy)) {
       ctx.scene.state.currancy = currancy;
-      ctx.reply(
+     await ctx.reply(
         "✍Write withdrawal address.\nBe careful, convince that your adress is correct:"
       );
       ctx.wizard.next();
     } else {
-      ctx.reply("⛔You must write currancy in this (format:ETH):");
+      await ctx.reply("⛔You must write currancy in this (format:ETH):");
       ctx.wizard.next();
     }
   },
-  (ctx) => {
+ async (ctx) => {
     const withdrawalAddress = ctx.message.text;
     ctx.scene.state.withdrawalAddress = withdrawalAddress;
-    ctx.reply("✍Write amount this currancy which you want to withdraw:");
+    await ctx.reply("✍Write amount this currancy which you want to withdraw:");
     ctx.wizard.next();
   },
   async (ctx) => {
@@ -34,24 +34,24 @@ export const withdrawScene = new Scenes.WizardScene(
       ctx.scene.state.amount = amount;
       ctx.wizard.next();
     } else {
-      ctx.reply("⛔You must write numbers");
+      await ctx.reply("⛔You must write numbers");
     }
   },
   async (ctx) => {
     const params = ctx.scene.state;
     const data = findUserInfo(ctx.from.id);
     try {
-      const res = await extractFromDB("usersKey", data);
+      const res = await DB("getData", data,"usersKey");
       await withdraw(
         res[0].apiSecret,
         res[0].apiKey,
         res[0].passPhrase,
         params
       );
-      ctx.reply("✅Operetion is succeed");
+     await ctx.reply("✅Operetion is succeed");
       ctx.scene.leave();
     } catch (err) {
-      ctx.reply(
+      await ctx.reply(
         `😓Sorry,something went wrong, make sure that the registration data is written correctly`
       );
       ctx.scene.leave();
